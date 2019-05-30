756f00c9973e12fbf7318cc2c1eac885773315ff
SunnyMarch <phamthuannsth@gmail.com>
2019-05-30 18:21:32 +0700

    Commit bai 6

---
+++ PecoWeb.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PecoWeb.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private PecoWebDbContext dbContext;
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+        public PecoWebDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
