e70e7e422dd3995646d60f3d6aa0e95c2a362250
SunnyMarch <phamthuannsth@gmail.com>
2019-05-31 10:03:02 +0700

    Commitbai 7

---
+++ PecoWeb.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PecoWeb.Data.Infrastructure
+{
+    class DbFactory : Disposable, IDbFactory
+    {
+        PecoWebDbContext dbContext;
+        public PecoWebDbContext Init()
+        {
+            return dbContext ?? (dbContext = new PecoWebDbContext());
+        }
+        protected override void DisposeCore()
+        {
+            if(dbContext != null)
+            {
+                dbContext.Dispose();
+            }
+        }
+    }
+}
---
+++ PecoWeb.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PecoWeb.Data.Infrastructure;
+using PecoWeb.Model.Models;
+
+namespace PecoWeb.Data.Repositories
+{
+    public interface IProductRepository
+    {
+
+    }
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
